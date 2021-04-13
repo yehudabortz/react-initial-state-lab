f83ab6ad95a47d0f8dd5a17b9e6ad51b784c08f8
Yehuda Bortz <yehudabortz@icloud.com>
2021-04-13 16:25:48 -0500

    commit tree 4923ba3be1401eec80876e00c937326cb1055bfc

--- src/Bomb.js
+++ src/Bomb.js
@@ -1,1 +1,19 @@
 // your Bomb code here!
+import React from "react";
+import { resetHistory } from "sinon";
+
+export default class Bomb extends React.Component {
+  constructor(props) {
+    super();
+    this.state = {
+      secondsLeft: props.initialCount,
+    };
+  }
+  render() {
+    const message =
+      this.state.secondsLeft === 0
+        ? "Boom!"
+        : `${this.state.secondsLeft} seconds left before I go boom!`;
+    return <div>{message}</div>;
+  }
+}
--- src/ImageSlider.js
+++ src/ImageSlider.js
@@ -1,1 +1,17 @@
-// your ImageSlider code here!
+import React from "react";
+
+export default class ImageSlider extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      currentSlideIndex: 0,
+    };
+  }
+  render() {
+    return (
+      <div>
+        <h1>I am on slide {this.state.currentSlideIndex}</h1>
+      </div>
+    );
+  }
+}
